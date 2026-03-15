1f5e5f1959d12d7984f121da18caca1a108c2c85
Alada <luchunning@inspirai.com>
2026-03-15 12:30:26 +0800

    UI component installation

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,5 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+
+// UI组件
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+// 额外引入图标库
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
+app.use(ArcoVue);
+app.use(ArcoVueIcon);
+
+
+createApp(App).use(router).mount('#app')
