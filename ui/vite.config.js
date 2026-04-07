402d19d5e198b5953a247d5fa1bc4e9b185756b3
Alada <luchunning@inspirai.com>
2026-04-08 01:33:14 +0800

    update

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     proxy: {
       // vite 类似与nginx, 后端 访问后端(没有浏览器的参与, 没有跨域的问题)
       // 要重启前端服务, 才会生效
-      '/api/vblog': {
+      '/api/blog': {
         target: 'http://127.0.0.1:7080'
       }
     }
